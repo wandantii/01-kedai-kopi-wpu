82857d915edd497f914a830201ea7ff8e74d695d
Wanda Berlian Mardianti <qr.wandantii@gmail.com>
2023-12-17 08:44:50 +0700

    First commit

---
+++ js/script.js
@@ -0,0 +1,14 @@
+// Toggle class active
+const navbarNav = document.querySelector('.navbar-nav');
+// Ketika humberger menu di klik
+document.querySelector('#hamburger-menu').onclick = () => {
+  navbarNav.classList.toggle('active');
+};
+
+// Klik di luar sidebar untuk menghilangkan sidebar
+const hamburger = document.querySelector('#hamburger-menu');
+document.addEventListener('click', function(e) {
+  if(!hamburger.contains(e.target) && !navbarNav.contains(e.target)) {
+    navbarNav.classList.remove('active');
+  }
+});
